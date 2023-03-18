5d46d58938d6af8be4ef5596ad42c2360ab1e39f
Shaja Azımı <110715621+SHJAx19@users.noreply.github.com>
2023-03-18 21:27:13 +0300

    Update RollingDice.java

--- Src/RollingDice.java
+++ Src/RollingDice.java
@@ -1,13 +1,19 @@
-package Src;
-
+/**
+ * The RollingDice class is a simple program that rolls a single six-sided die
+ * and prints the result.
+ */
 public class RollingDice {
+    /**
+     * main method
+     * 
+     * @param args
+     */
     public static void main(String[] args) {
-        // Test the Dice
-        Die die = new Die();
-        System.out.println("Initial face value of die: " + die.getFaceValue());
+        Die die = new Die(); // My object
+        System.out.println("Initial face value of die: " + die.getFaceValue()); // testing my dice
         System.out.println("----------------------------------------------------------------");
         die.roll();
-        System.out.println("After rolling dice: " + die.getFaceValue());
+        System.out.println("After rolling dice: " + die.getFaceValue()); // result after rolling the dice
         System.out.println("----------------------------------------------------------------");
     }
 }
