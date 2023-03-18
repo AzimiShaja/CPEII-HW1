4f176a12996aeca3381ccf902286ae2cefd285fe
Shaja Azımı <110715621+SHJAx19@users.noreply.github.com>
2023-03-18 21:26:52 +0300

    Documented

--- Src/PairOfDice.java
+++ Src/PairOfDice.java
@@ -1,52 +1,95 @@
-package Src;
-
+/**
+ * The PairOfDice class represents a pair of six-sided dice that can be rolled
+ * and have their face values set or retrieved.
+ */
 public class PairOfDice {
     private Die die1;
     private Die die2;
 
-    // Constructor
+    /**
+     * Creates a new PairOfDice object with two dice that are rolled.
+     */
     public PairOfDice() {
         die1 = new Die();
         die2 = new Die();
     }
 
-    // Roll the dice
+    /**
+     * Rolls both dice.
+     */
     public void roll() {
         die1.roll();
         die2.roll();
     }
 
-    // Get the sum of the dice
+    /**
+     * Returns the sum of the face values of both dice.
+     * 
+     * @return the sum of the face values of both dice
+     */
     public int getDiceSum() {
         return die1.getFaceValue() + die2.getFaceValue();
     }
 
-    // Get the face value of die1
+    /**
+     * 
+     * Returns the face value of the first die.
+     * 
+     * @return the face value of the first die
+     */
     public int getDie1() {
         return die1.getFaceValue();
     }
 
-    // Set the face value of die1
+    /**
+     * 
+     * Sets the face value of the first die to the specified value.
+     * 
+     * @param value the value to set the face value of the first die to
+     */
     public void setDie1(int value) {
         die1.setFaceValue(value);
     }
 
-    // Get the face value of die2
+    /**
+     * 
+     * Returns the face value of the second die.
+     * 
+     * @return the face value of the second die
+     */
     public int getDie2() {
         return die2.getFaceValue();
     }
 
-    // Set the face value of die2
+    /**
+     * 
+     * Sets the face value of the second die to the specified value.
+     * 
+     * @param value the value to set the face value of the second die to
+     */
     public void setDie2(int value) {
         die2.setFaceValue(value);
     }
 
-    // String representation of the dice
+    /**
+     * 
+     * Returns a string representation of the face values of both dice.
+     * 
+     * @return a string representation of the face values of both dice
+     */
     public String toString() {
         return die1.getFaceValue() + " " + die2.getFaceValue();
     }
 
-    // equals Method
+    /**
+     * 
+     * Compares this pair of dice to another pair of dice to see if they have the
+     * same face values.
+     * 
+     * @param other the other pair of dice to compare to
+     * @return true if the two pairs of dice have the same face values, false
+     *         otherwise
+     */
     public boolean equals(PairOfDice other) {
         return die1.equals(other.die1) && die2.equals(other.die2);
     }
